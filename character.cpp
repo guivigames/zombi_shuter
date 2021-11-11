78a6b2eb9a5c4f381a8f969cceb97678241fbf14
guivigames <games@guivi.one>
2021-11-11 07:33:56 +0000

    Completed function on the character class

--- character.cpp
+++ character.cpp
@@ -5,6 +5,10 @@
 /// @brief
 Character::Character()
 {
+    m_Speed     = START_SPEED;
+    m_Health    = START_HEALTH;
+    m_MaxHealth = START_HEALTH;
+
     sf::Texture texture  = TheTextureManager::Instance()->GetTexture("assets/player_walk_strip6.png");
     m_walk_frames = 6;
     m_walk_size = texture.getSize();
@@ -23,10 +27,163 @@ Character::~Character()
 
 }
 
+void Character::Spawn(sf::IntRect arena, sf::Vector2f resolution, int tileSize)
+{
+    m_position.x = arena.width/2;
+    m_position.y = arena.height/2;
+    m_Arena = arena;
+
+    m_TileSize = tileSize;
+
+    m_resolution = resolution;
+}
+
+void Character::resetPlayerState()
+{
+    m_Speed = START_SPEED;
+    m_Health = START_HEALTH;
+    m_MaxHealth = START_HEALTH;
+}
+
+sf::Time Character::getLastHitTime()
+{
+    return m_LastHit;
+}
+
+bool Character::hit(sf::Time timeHit)
+{
+    if (timeHit.asMilliseconds() - m_LastHit.asMilliseconds() > 200)
+    {
+        m_LastHit = timeHit;
+        m_Health -= 10;
+        return true;
+    }
+    else return false;
+}
+
+sf::FloatRect Character::getPosition()
+{
+    return m_walk_sprite.getGlobalBounds();
+}
+
+sf::Vector2f Character::getCenter()
+{
+    return m_position;
+}
+
+float Character::getRotation()
+{
+    return m_walk_sprite.getRotation();
+}
+
 /// @brief
 sf::Sprite Character::GetSprite()
 {
     return m_walk_sprite;
+}
+
+int Character::getHealth()
+{
+    return m_Health;
+}
+
+void Character::moveLeft()
+{
+    m_LeftPressed = true;
+}
+
+void Character::moveRight()
+{
+    m_RightPressed = true;
+}
+
+void Character::moveUp()
+{
+    m_UpPressed = true;
+}
+
+void Character::moveDown()
+{
+    m_DownPressed = true;
+}
+
+void Character::stopLeft()
+{
+    m_LeftPressed = false;
+}
+
+void Character::stopRight()
+{
+    m_RightPressed = false;
+}
+
+void Character::stopUp()
+{
+    m_UpPressed = false;
+}
+
+void Character::stopDown()
+{
+    m_DownPressed = false;
+}
+
+void Character::update( float elapsedTime, sf::Vector2f mousePosition)
+{
+    if(m_UpPressed)
+    {
+        m_position.y -= m_Speed * elapsedTime;
+    }
+
+    if(m_DownPressed)
+    {
+        m_position.y += m_Speed * elapsedTime;
+    }
+
+    if(m_LeftPressed)
+    {
+        m_position.x -= m_Speed * elapsedTime;
+    }
+
+    if(m_RightPressed)
+    {
+        m_position.x += m_Speed * elapsedTime;
+    }
+
+    m_walk_sprite.setPosition(m_position);
+
+    if (m_position.x > m_Arena.width - m_TileSize)
+        m_position.x = m_Arena.width - m_TileSize;
+
+    if (m_position.x < m_Arena.width + m_TileSize)
+        m_position.x = m_Arena.width + m_TileSize;
+    
+    if (m_position.y > m_Arena.height - m_TileSize)
+        m_position.y = m_Arena.height - m_TileSize;
+    
+    if (m_position.y < m_Arena.height + m_TileSize)
+        m_position.y = m_Arena.height + m_TileSize;
+
+
+    float angle = (atan2(   mousePosition.y - m_resolution.y / 2, 
+                            mousePosition.x - m_resolution.x / 2) * 180) / 3.1415;
+    m_walk_sprite.setRotation(angle);
+}
+
+void Character::upgradeSpeed()
+{
+    m_Speed += (START_SPEED * 0.2);
+}
+
+void Character::upgradeHealth()
+{
+    m_MaxHealth += (START_HEALTH * 0.2);
+}
+
+void Character::increaseHealthLevel(int amount)
+{
+    m_Health+=amount;
+    if (m_Health > m_MaxHealth)
+        m_Health = m_MaxHealth;
 }
 
 /// @brief
@@ -98,3 +255,4 @@ bool Character::WillUpdate()
     }
     return m_will_update;
 }
+
--- character.h
+++ character.h
@@ -8,20 +8,54 @@
 class Character
 {
 protected:
+    const float START_SPEED = 200;
+    const float START_HEALTH = 100;
+    float           m_Speed;
     sf::Vector2f    m_position = { 0.0, 0.0};   // Position of character.
     sf::Vector2f    m_direction = { 1.0, 0.0};  // Direction of character.
     sf::Vector2f    m_velocity = { 0.0, 0.0};   // Velocit of character.
+    sf::Vector2f    m_resolution;               // Screen resolution.
     sf::Sprite      m_walk_sprite;              // Sprites of chracter while walking. 
     sf::Vector2u    m_walk_size;                // Size of the entire texture.
     sf::IntRect     m_walk_frame;               // Rectangle of texture to render.
     id_t            m_walk_frames;              // total number of sprites in texture.
+    sf::IntRect     m_Arena;                    // Size of the current arena.
+    int             m_TileSize;                 // How big is each tile in the arena.
+    int             m_Health;
+    int             m_MaxHealth;
     sf::Sprite      m_run_sprite;               // Sprite for running.
     sf::Clock       m_walk_clock;               // Clock for updating the sprites.
     bool            m_will_update = false;      // Flag to indicate if the player will update.
+    bool            m_UpPressed = false;        // Flag to indicate if the up button is pressed.
+    bool            m_DownPressed = false;
+    bool            m_LeftPressed = false;
+    bool            m_RightPressed = false;
+    sf::Time        m_LastHit;
 
 public:
     Character();
     ~Character();
+    void Spawn(sf::IntRect arena, sf::Vector2f resolution, int tileSize);
+    void resetPlayerState();
+    bool hit(sf::Time timeHit);
+    sf::Time getLastHitTime();
+    sf::FloatRect getPosition();
+    sf::Vector2f getCenter();
+    float getRotation();
+    sf::Sprite getSprite();
+    void moveLeft();
+    void moveRight();
+    void moveUp();
+    void moveDown();
+    void stopLeft();
+    void stopRight();
+    void stopUp();
+    void stopDown();
+    void update(float elapsedTime, sf::Vector2f mousePosition);
+    void upgradeSpeed();
+    void upgradeHealth();
+    void increaseHealthLevel(int amount);
+    int getHealth();
     //void SetSprite(std::string texture);
     sf::Sprite GetSprite();
     void SetPosition(sf::Vector2f pos);
